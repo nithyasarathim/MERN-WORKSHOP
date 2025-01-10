8b6a6166922f6b5d89a4ad021042f62d7c03e1e5
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2025-01-10 12:55:09 +0530

    API project done

--- DAY-5/jokes/index.js
+++ DAY-5/jokes/index.js
@@ -1,6 +1,12 @@
 const btn= document.getElementById('btn');
 const jokeques= document.getElementById('jokeques');
 const jokeans= document.getElementById('jokeans');
+const quoteBtn = document.getElementById('quoteBtn');
+const quote = document.getElementById('quote');
+const showDogsBtn = document.getElementById('dogBtn');
+const dogImage = document.getElementById('dogImage');
+const dogQuote = document.getElementById('dog');
+
 btn.onclick=()=>{
     axios.get('https://official-joke-api.appspot.com/random_joke')
     .then(function(res){
@@ -14,3 +20,31 @@ btn.onclick=()=>{
         console.log(err);
     });
 }
+
+quoteBtn.onclick = () => { 
+    axios.get('https://api.adviceslip.com/advice')
+        .then(function(res) {
+            quoteBtn.textContent = "Next";
+            quote.innerHTML = res.data.slip.advice;
+        })
+        .catch(function(err) {
+            console.log(err);
+            quote.innerHTML = "Could not fetch a quote. Please try again later.";
+        });
+}
+
+showDogsBtn.onclick = () => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+        .then(function(res) {
+            showDogsBtn.textContent = "Show Another Dog";
+            dogQuote.innerHTML = "Here's a cute dog for you!";
+            dogImage.src = res.data.message;
+            dogImage.style.display = "block";
+        })
+        .catch(function(err) {
+            console.log(err);
+            dogQuote.innerHTML = "Could not fetch a dog image. Please try again later.";
+            dogImage.style.display = "none";
+        });
+}
+
